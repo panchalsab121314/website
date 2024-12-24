7d6ea55c368b80c84d2516dec0c431a11627c735
lalit <panchalsab121314@gmail.com>
2024-12-24 12:26:02 +0530

    edd

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let hello = document.getElementById("hello")
 let slider_complete = document.getElementById("slider_complete")
 
 
-hello.addEventListener('click',()=>{
-    bar.style.display='block'
-    slider_complete.style.display='none'
+hello.addEventListener('click', () => {
+    bar.style.display = 'block'
+    slider_complete.style.display = 'none'
 })
 cross.addEventListener('click', () => {
     bar.style.display = 'none'
-     slider_complete.style.display='block'
+    slider_complete.style.display = 'block'
 })
 
 
@@ -22,13 +22,13 @@ let num = 0;
 const set_time = setInterval(() => {
     slider_fun(num++)
 }, 2000)
-function slider_fun(index,i) {
+function slider_fun(index, i) {
     if (index == slide.length) {
         index = 0
         num = index
     }
     if (index == -1) {
-        index = slide.length - 1 
+        index = slide.length - 1
         num = index
     }
     slider.style.transform = `translateX(-${index}00%)`
@@ -42,6 +42,7 @@ let text_mouse = document.querySelectorAll('text_mouse')
 let Jackets = document.querySelectorAll('Jackets')
 
 input.addEventListener('input', () => {
+    let display_none_before_serch = document.querySelector('.display_none_before_serch')
     let data = input.value
     console.log(data)
     for (let i = 0; i < mouse.length; i++) {
@@ -49,24 +50,26 @@ input.addEventListener('input', () => {
         if (h2.toLocaleLowerCase().includes(data.toLocaleLowerCase())) {
             mouse[i].style.display = "block"
         } else {
-        mouse[i].style.display = "none"
+            mouse[i].style.display = "none"
+             display_none_before_serch.style.display = "none"
         }
     }
 })
-
+// there areparts off inpute serchbar in phone formet
+let display_none_before_serch = document.querySelector('.display_none_before_serch')
 let typeing = document.getElementById('typeing')
-
-typeing.addEventListener('input',()=>{
-    let data=typeing.value
+typeing.addEventListener('input', () => {
+    let data = typeing.value
     console.log(data)
     for (let i = 0; i < mouse.length; i++) {
         const h2 = mouse[i].getElementsByTagName('h2')[0].textContent;
         if (h2.toLocaleLowerCase().includes(data.toLocaleLowerCase())) {
             mouse[i].style.display = "block"
         } else {
-        mouse[i].style.display = "none"
+            mouse[i].style.display = "none"
+            display_none_before_serch.style.display = "none"
         }
     }
-    
+
 })
 
